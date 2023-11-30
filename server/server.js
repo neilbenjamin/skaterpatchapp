b0bd9058240524e759aa8a310d5ec8f353ca78c3
neilbenjamin <neilbenjaminmusic@gmail.com>
2023-11-30 23:35:03 +0200

    Added routing middlware

--- server/server.js
+++ server/server.js
@@ -15,9 +15,12 @@ app.use(express.json());
 // Use the cors middleware to handle Cross-Origin Resource Sharing (CORS)
 app.use(cors());
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../client/build')));
+
 // Base server route
 app.get('/', (req, res) => {
-  res.send('Welcome, the server is active.');
+  res.send('Welcome, the server is active, bud.');
 });
 
 // Health route
@@ -29,6 +32,7 @@ app.get('/health', (req, res) => {
 const userRouter = require('./routers/userRouter'); // Adjust the path as necessary
 app.use('/api/users', userRouter); // Use userRouter for user-related routes
 
+
 // //production script
 // app.use(express.static('./client/frontend/build'));
 // app.get("*", (req,res)=> {
@@ -36,10 +40,17 @@ app.use('/api/users', userRouter); // Use userRouter for user-related routes
 // });
 
 
-// Listening on port 8080.
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+// The "catchall" handler: for any request that doesn't
+// match one above, send back the index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
+
+const port = process.env.PORT || 10000;
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
+
+
 // Export the server for testing
 module.exports = app;
