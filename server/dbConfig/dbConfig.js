c878d9e6f4419f1d5d503f05d6f35537f39f1351
neilbenjamin <neilbenjaminmusic@gmail.com>
2023-11-30 06:57:15 +0200

    Update dB Config remove password

--- server/dbConfig/dbConfig.js
+++ server/dbConfig/dbConfig.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose');
 //Mongodg authentication string obtained from website VSC section. 
-// const dbURL = 'mongodb+srv://neilbenjamin:GuitarGod69@cardatabase.oz5suw9.mongodb.net/';
 const dbURL = process.env.DB_URL; // Use environment variable for DB URL
-const dbName = process.env.DB_NAME; // Optionally, use an environment variable for DB name
-
+// const dbName = process.env.DB_NAME; // Optionally, use an environment variable for DB name
 
 mongoose.connect(dbURL, {
   useNewUrlParser: true,
