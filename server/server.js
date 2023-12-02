f8c72e84a0c1a7eaa34017fd60bd9b40b862abaa
neilbenjamin <neilbenjaminmusic@gmail.com>
2023-12-02 14:15:44 +0200

    Updated SErver pors

--- server/server.js
+++ server/server.js
@@ -9,14 +9,20 @@ const app = express();
 const path = require('path');
 app.use(cookieParser());
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+
 // Middleware to parse JSON data
 app.use(express.json());
 
 // Use the cors middleware to handle Cross-Origin Resource Sharing (CORS)
 app.use(cors());
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, '../client/build')));
+
+// New Registered User Routers
+const userRouter = require('./routers/userRouter'); // Adjust the path as necessary
+app.use('/api/users', userRouter); // Use userRouter for user-related routes
 
 // Base server route
 app.get('/', (req, res) => {
@@ -28,25 +34,14 @@ app.get('/health', (req, res) => {
   res.json({ message: 'server is healthy' }); // Return JSON response
 });
 
-// New Registered User Routers
-const userRouter = require('./routers/userRouter'); // Adjust the path as necessary
-app.use('/api/users', userRouter); // Use userRouter for user-related routes
 
-
-// //production script
-// app.use(express.static('./client/frontend/build'));
-// app.get("*", (req,res)=> {
-//   res.sendFile(path.resolve(__dirname, "client", "frontend", "build", "index.html"))
-// });
-
-
-// The "catchall" handler: for any request that doesn't
-// match one above, send back the index.html file.
+// // The "catchall" handler: for any request that doesn't
+// // match one above, send back the index.html file.
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
-const port = process.env.PORT || 10000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
